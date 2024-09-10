7be7b64b1debc51aacbb1befbb0db8ffb44ac912
Zhiqin-Wei <zhiqin.daly.wei@mths.ca>
2024-09-10 10:49:56 -0400

    Daly

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Daly
+ * Created on:sept 2024
+ * This program said Hello, Daly
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, Daly!')
